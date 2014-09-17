c0594a94d96c8a3bdc2e8b1b1f7a7845535e7e8a
Vikram Middha <vikram.middha@makepositive.com>
2014-09-17 13:29:43 +0530

    added feeds layout

--- public/awardsforceui/www/js/controllers/home-controller.js
+++ public/awardsforceui/www/js/controllers/home-controller.js
@@ -1,10 +1,23 @@
 angular.module('home-controller' , [])
 
 .controller('homeController', function($scope ,  $ionicModal, $ionicPopup , $timeout) {
-	$scope.contact = {
-
-		FirstName : 'Vikram',
-		LastName : 'Middha'
-	};
+	$scope.Feeds = {
+		"001" : {
+			"Giver" : "Vikram Middha",
+			"Taker" : "Gulshan Middha",
+			"Time" : "01/01/2014 12:00:00",
+			"LikesCount" : "101",
+			"CommentsCount" : "22",
+			"Description" : "Good job done"
+		},
+		"002" : {
+			"Giver" : "Vikram Middha",
+			"Taker" : "Vikas Malhotra",
+			"Time" : "01/01/2014 12:00:00",
+			"LikesCount" : "12",
+			"CommentsCount" : "20",
+			"Description" : "Great Job"
+		}
+	}
 });
 
